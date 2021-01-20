d8b937320cbd8cdb160b07cc488136b2b1072e97
Bhagyatrivedi27 <bhagyatrivedi20273@gmail.com>
2021-01-20 15:16:57 +0530

    Private Route for Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,8 @@ import Login from './components/auth/Login'
 import Alert from './components/layout/Alert'
 import {loadUser} from './actions/auth' 
 import setAuthToken from './utils/setAuthToken'
-
+import Dashboard from './components/dashboard/Dashboard'
+import PrivateRoute from './components/routing/PrivateRoute'
 //Redux 
 import {Provider} from 'react-redux'
 import store from './store'
@@ -32,6 +33,7 @@ const App = () => {
     <Switch>
       <Route exact path = "/register" component={Register}></Route>
       <Route exact path = "/login" component={Login}></Route>
+      <PrivateRoute exact path = "/dashboard" component={Dashboard}></PrivateRoute>
       </Switch>  
     </section>
   </Fragment>
