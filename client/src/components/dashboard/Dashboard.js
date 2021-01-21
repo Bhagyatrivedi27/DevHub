10ce2889be14440b5f7e4ddc61f6b198cf4c4459
Bhagyatrivedi27 <bhagyatrivedi20273@gmail.com>
2021-01-21 21:11:55 +0530

    Delete functionality added

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,12 +2,12 @@ import React, {Fragment, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {connect } from 'react-redux'
-import {getCurrentProfile} from '../../actions/profile'
+import {deleteAccount, getCurrentProfile} from '../../actions/profile'
 import Spinner from '../layout/Spinner'
 import DashboardActions from './DashboardActions'
 import Experience from './Experience'
 import Education from './Education'
-const Dashboard = ({getCurrentProfile, auth:{user}, profile: {profile, loading}}) => {
+const Dashboard = ({getCurrentProfile,deleteAccount, auth:{user}, profile: {profile, loading}}) => {
 
     useEffect(()=>{
         getCurrentProfile()
@@ -22,6 +22,11 @@ const Dashboard = ({getCurrentProfile, auth:{user}, profile: {profile, loading}}
         {profile !==null ? <Fragment><DashboardActions></DashboardActions>
         <Experience experience={profile.experience}></Experience>
         <Education education={profile.education}></Education>
+        <div className="my-2">
+            <button className="btn btn-danger" onClick={() => deleteAccount()}>
+                <i className="fas fa-user-minus"></i> Delete Account
+            </button>
+        </div>
         </Fragment> : <Fragment>
             <p>You have not yet setup a profile, please add some info </p>
             <Link to ='/create-profile' className="btn btn-primary my-1">Create Profile</Link>
@@ -37,11 +42,12 @@ Dashboard.propTypes = {
     getCurrentProfile: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
     profile: PropTypes.object.isRequired,
+    deleteAccount: PropTypes.func.isRequired,
 }
 
 const mapStateToProps = state => ({
     auth: state.auth,
-    profile: state.profile
+    profile: state.profile 
 })
 
-export default connect(mapStateToProps, {getCurrentProfile})(Dashboard)
+export default connect(mapStateToProps, {getCurrentProfile, deleteAccount})(Dashboard)
--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -2,9 +2,11 @@ import React, {Fragment} from 'react'
 import PropTypes from 'prop-types'
 import Moment from 'react-moment'
 import {connect} from 'react-redux'
+import {deleteExperience} from '../../actions/profile'
+import { profile_url } from 'gravatar'
 
 
-const Experience = ({ experience }) => {
+const Experience = ({ experience, deleteExperience }) => {
     const experiences = experience.map(exp =>(
         <tr key={exp.id}>
             <td>{exp.company}</td>
@@ -16,7 +18,7 @@ const Experience = ({ experience }) => {
                 <Moment format='YYYY/MM/DD'>{exp.to}</Moment>
             )}
             </td>
-            <td><button className='btn btn-danger'>Delete</button></td>
+            <td><button onClick={() => deleteExperience(exp._id)} className='btn btn-danger'>Delete</button></td>
         </tr>
     ))
     return (
@@ -38,6 +40,7 @@ const Experience = ({ experience }) => {
 
 Experience.propTypes = {
     experience: PropTypes.array.isRequired,
+    deleteExperience: PropTypes.func.isRequired,
 }
 
-export default Experience
+export default connect(null,{deleteExperience})(Experience)
